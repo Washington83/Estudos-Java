76d1c7e6d4e2d312e6ffe4f77b872ddb677896f5
Washington Costa <costasantana980@gmail.com>
2026-07-01 19:57:57 -0300

    Polimorfismo

---
+++ Java-Curso/src/Polimorfismo/App.java
@@ -0,0 +1,17 @@
+package Polimorfismo;
+
+import Polimorfismo.devFlix.Filme;
+import Polimorfismo.devFlix.Video;
+
+public class App {
+    static void main(String[] args) {
+        // Polimorfismo
+        // poli -> muitas
+        // morfo -> múltiplas formas
+
+        // Permite que o mesmo nome, represente vários compartamentos diferente
+
+        // um unico nome -> pode ter comportamentos diferentes
+           Filme filme = new Filme("Null");
+;    }
+}
